ebd157aa822827dcfd7f5d4bf2b422d368c62681
khanhdang <dnk0904@gmail.com>
2026-09-07 23:49:38 +0900

    Update tb_SECDED_1622_dec.v

--- tb/tb_SECDED_1622_dec.v
+++ tb/tb_SECDED_1622_dec.v
@@ -9,7 +9,7 @@ module tb_SECDED_1622_dec;
     // Simulation parameters
     // ============================================================
     parameter CLOCK_PERIOD = 10;       // ns, 100 MHz
-    parameter NUM_CYCLES   = 100000;   // number of codewords
+    parameter NUM_CYCLES   = 100;   // number of codewords
 
     reg clk;
     reg rst_n;
@@ -22,6 +22,9 @@ module tb_SECDED_1622_dec;
     wire uncorrected;
 
     integer i;
+
+    integer num_errors;
+    integer pos1, pos2;
 
     // ============================================================
     // Encoder
@@ -55,6 +58,33 @@ module tb_SECDED_1622_dec;
         forever #(CLOCK_PERIOD/2) clk = ~clk;
     end
 
+
+
+    // ============================================================
+    // Error Mask
+    // ============================================================
+    always @(*) begin
+        error_mask = CODE_BIT'b0;
+
+        //num_errors = $urandom_range(0, 2);
+
+        if (num_errors == 1) begin
+            pos1 = $urandom_range(0, CODE_BIT-1);
+            error_mask[pos1] = 1'b1;
+        end
+
+        else if (num_errors == 2) begin
+            pos1 = $urandom_range(0, CODE_BIT-1);
+
+            do begin
+                pos2 = $urandom_range(0, CODE_BIT-1);
+            end while (pos2 == pos1);
+
+            error_mask[pos1] = 1'b1;
+            error_mask[pos2] = 1'b1;
+        end
+    end
+
     // ============================================================
     // Test
     // ============================================================
@@ -63,7 +93,7 @@ module tb_SECDED_1622_dec;
         // Initial condition
         rst_n = 1'b0;
         data  = '0;
-        error_mask = '0;
+        num_errors = 0;
 
         // Reset
         #(2 * CLOCK_PERIOD);
@@ -79,6 +109,11 @@ module tb_SECDED_1622_dec;
 
             data = $urandom;
 
+            if (i>NUM_CYCLES/3) begin
+                num_errors = 1;
+            end if  (i>NUM_CYCLES*2/3) begin
+                num_errors = 2;
+            end
         end
 
         // Hold last value
